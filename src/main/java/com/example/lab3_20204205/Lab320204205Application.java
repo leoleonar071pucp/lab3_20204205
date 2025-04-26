e06fec7bf6f01461360ba6d0699eaaa843e99693
dodo240703 <149447486+dodo240703@users.noreply.github.com>
2025-04-25 20:26:08 -0500

    first commit

---
+++ src/main/java/com/example/lab3_20204205/Lab320204205Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20204205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320204205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320204205Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20204205/Lab320204205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20204205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320204205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
