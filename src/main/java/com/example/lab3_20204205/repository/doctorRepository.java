76c18fe45d058882e38935a02a1596c8754fe21c
leoleonar071 <109324448+leoleonar071@users.noreply.github.com>
2025-05-12 09:18:54 -0500

    update12.05

--- src/main/java/com/example/lab3_20204205/repository/doctorRepository.java
+++ src/main/java/com/example/lab3_20204205/repository/doctorRepository.java
@@ -1,6 +1,7 @@
 package com.example.lab3_20204205.repository;
 
 import com.example.lab3_20204205.entity.doctor;
+import com.example.lab3_20204205.entity.paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,11 +14,9 @@ public interface doctorRepository extends JpaRepository<doctor, Integer> {
 
 
 
-    @Query("""
-        SELECT  * FROM paciente  where paciente.doctor_id = ?1
-        
-    """)
-    List<doctor> listarPacientesPorDoctor(int doctorId);
+    @Query("SELECT p FROM paciente p WHERE p.doctor.id = ?1")
+    List<paciente> listarPacientesPorDoctor(int doctorId);
+
 
 
 
